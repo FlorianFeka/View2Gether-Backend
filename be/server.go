4edd6cd622b65b2a43f9fd24f385b6f84295690c
FlorianFeka <florianfeka05@gmail.com>
2020-03-22 18:37:42 +0100

    add basic socket io server

---
+++ be/server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/googollee/go-socket.io"
+)
+
+func forEach(s socketio.Conn) {
+	s.Emit("The for each function called")
+}
+
+func main() {
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	server.OnConnect("/", func(s socketio.Conn) error {
+
+		server.JoinRoom("", "test", s)
+
+		fmt.Println("connected ID:", s.ID())
+		fmt.Println("connected ROOMS:", s.Rooms())
+		fmt.Println("connected URL:", s.URL())
+		return nil
+	})
+	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
+		server.ForEach("" ,"test", forEach)
+		fmt.Println("notice:", msg)
+		s.Emit("reply", "have "+msg)
+	})
+	server.OnError("/", func(s socketio.Conn, e error) {
+		fmt.Println("meet error:", e)
+	})
+	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+		fmt.Println("closed", reason)
+	})
+	go server.Serve()
+	defer server.Close()
+
+	http.Handle("/", server)
+	log.Println("Serving at localhost:8000...")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
