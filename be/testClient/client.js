98d14012a222e4196b5ac557a063bd5da52ab839
FlorianFeka <florianfeka05@gmail.com>
2020-07-21 13:52:05 +0200

    add Send Info for client sync when joining room

--- be/testClient/client.js
+++ be/testClient/client.js
@@ -1,27 +1,49 @@
 const io = require('socket.io-client');
 
-var socket = io.connect('http://localhost:8000');
+const socket1 = io.connect('http://localhost:8000');
+const socket2 = io.connect('http://localhost:8000');
+const testRoom = 'test_room';
 
-joinRoom();
-sendCommandToRoom('test_command');
-sendCommandToRoom('play_command');
+joinRoom(socket1, testRoom);
+joinRoom(socket2, testRoom);
+sendCommandToRoom(socket1, testRoom, 'test_command');
 
-function joinRoom() {
-    console.log(' ####### Join room ####### ');
-    socket.emit('joinRoom', '1234');
+// sendCommandToRoom('test_command');
+// sendCommandToRoom('play_command');
+
+function joinRoom(socket, roomId) {
+    const timestamp = new Date();
+    console.log(`[${timestamp.toISOString()}] - Join Room:
+    ${roomId}`);
+    socket.emit('joinRoom', roomId);
 }
 
-function sendCommandToRoom(command) {
-    console.log(' ####### Second command ####### ');
-    socket.emit('sendCommandToRoom', '1234', command);
+function getInfo(socket, roomId) {
+    const timestamp = new Date();
+    console.log(`[${timestamp.toISOString()}] - Get Info`);
+    socket.on(roomId).emit("getInfo");    
 }
 
-socket.on('command', function (data) {
-    console.log('Broadcast #test command# received');
-    console.log(data);
-});
+function sendInfo(socket, roomId, info) {
+    const timestamp = new Date();
+    console.log(`[${timestamp.toISOString()}] - Info:
+    ${JSON.stringify(info, null, 4)}`);
+    socket.on(roomId).emit("info", info)
+}
 
-socket.on('command', function (data) {
-    console.log('Broadcast #play command# received');
-    console.log(data);
-});
+function sendCommandToRoom(socket, roomId, command) {
+    const timestamp = new Date();
+    console.log(`[${timestamp.toISOString()}] - Send Command to Room (${roomId}):
+    ${JSON.stringify(command, null, 4)}`);
+    socket.emit('sendCommandToRoom', roomId, command);
+}
+
+// socket.on('command', function (data) {
+//     console.log('Broadcast #test command# received');
+//     console.log(data);
+// });
+
+// socket.on('command', function (data) {
+//     console.log('Broadcast #play command# received');
+//     console.log(data);
+// });
--- be/testServer/server.js
+++ be/testServer/server.js
@@ -1,5 +1,7 @@
 var io = require("socket.io")(8000);
 
+console.log("Socket setup started ...");
+
 io.on("connection", (socket) => {
   socket.on("joinRoom", (roomId) => {
     const timestamp = new Date();
@@ -8,18 +10,29 @@ io.on("connection", (socket) => {
     socket.join(roomId);
   });
 
-  socket.on("getInfo", (roomId, info) => {
+  socket.on("getInfo", (roomId) => {
     const timestamp = new Date();
     console.log(`[server.js] ${timestamp.toISOString()} Get Info`);
+    socket.to(roomId).emit("getInfo");
   });
 
-  socket.on("sendCommandToRoom", (room, command) => {
+  socket.on("sendInfo", (roomId, info) => {
+    const timestamp = new Date();
+    console.log(`[server.js] ${timestamp.toISOString()} Send Info:
+    ${JSON.stringify(info, null, 4)}`);
+
+    io.to(roomId).emit("info", info);
+  });
+
+  socket.on("sendCommand", (roomId, command) => {
     const timestamp = new Date();
     console.log(
       `[server.js] ${timestamp.toISOString()} Command Sent: 
       ${JSON.stringify(command, null, 4)}`
     );
 
-    io.to(room).emit("command", command);
+    io.to(roomId).emit("command", command);
   });
 });
+
+console.log("Socket setup finished ...");
